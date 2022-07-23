0e7b22a96b586a2345107f7ac5df7107db4c3078
Aaron Rodi <metalhead5209@gmail.com>
2022-07-23 11:03:51 -0500

    added state to render input from todo form

--- src/App.tsx
+++ src/App.tsx
@@ -5,15 +5,22 @@ import Todos from './components/Todos/Todos'
 import Todo from './model/todo'
 import { useState } from 'react';
 
+
+
 function App() {
-  const todos = [
-    new Todo('Learn React'),
-    new Todo('Learn Typescript')
-  ]
+  const [todos, setTodos] = useState<Todo[]>([]);
+
+  const addTodoHandler = (text: string) => {
+    const newTodo = new Todo(text);
+
+    setTodos((prevTodos) => {
+      return prevTodos.concat(newTodo);
+    })
+  }
 
   return (
     <div className="App">
-      <TodoForm />
+      <TodoForm onAddTodo={addTodoHandler} />
       <Todos items={todos}/>
     </div>
   )
