bb567fb05db07fe985dc728840dcef1942c621bd
Aaron Rodi <metalhead5209@gmail.com>
2022-07-23 08:14:19 -0500

    set up todo component

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+
+import './App.css'
+import Todos from './Todos/Todos'
+
+function App() {
+
+  return (
+    <div className="App">
+      <Todos items={['Learn React', 'Learn Typescrpt']}/>
+    </div>
+  )
+}
+
+export default App
